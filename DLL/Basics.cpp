03b4b7bde4b88aecb7313646a3e600ebeefe6803
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-05 09:35:05 +0530

    changing dll

--- DLL/Basics.cpp
+++ DLL/Basics.cpp
@@ -1,159 +1,170 @@
 
 
-//https://ide.geeksforgeeks.org/b820b3c8-9939-4146-a218-5c96055415e6
+// https://ide.geeksforgeeks.org/9ca06a66-6c4e-4823-956e-ac1dcbe63f56
+
+
 #include <iostream>
 using namespace std;
 
-class Node
-{
 
-public:
+
+class Node{
+    public:
+    
     int data;
+    Node *prev;
     Node *next;
-
+    
     Node(int data)
     {
-        this->data = data;
-        this->next = NULL;
+        this->data=data;
+        next=NULL;
+        prev=NULL;
+        
     }
-};
+}
+;
 
-class LinkList{
+
+class DoubllyLinkList{
     
     public:
-
-    void insertAtHead(Node *&head, int d)
-    {
-
-        Node *temp = new Node(d);
-        temp->next = head;
-        head = temp;
-    }
     
-    void insertEnd(Node *&head,int data)
+    void insertATHead(Node *&head,int data)
+    {
+        Node *newNode=new Node(data);
+        newNode->next=head;
+        head->prev=newNode;
+        head=newNode;
+        
+    }
+    void insertAtEnd(Node *&head,int data)
     {
         Node *newNode =new Node(data);
-        
-        if(head==NULL)
-        {
-            insertAtHead(head,data);
-            return;
-        }
-        
         Node *temp=head;
-        while(temp->next!=NULL)
+         while(temp->next!=NULL)
         {
             temp=temp->next;
         }
         temp->next=newNode;
+        newNode->prev=temp;
+        
+       
+        
     }
-    
-    void insertAtPos(Node *&head,int pos,int data)
+
+    void insertAtEndWithTail(Node *&tail,int data)
     {
-        Node *temp=head;
+         Node *newNode =new Node(data);
+         tail->next = newNode;
+         newNode->prev = tail;
+    }
+    void insertATPos(Node *&head,int data,int pos)
+    {
+        
         if(pos==1)
         {
-            insertAtHead(head,data);
+            insertATHead(head,data);
             return;
+            
         }
+        Node *temp=head;
+        Node *newNode =new Node (data);
         int count=1;
+        
+        
         while(count <pos-1)
         {
             temp=temp->next;
             count++;
         }
+        if(temp->next==NULL  || temp==NULL)
+        {
+            insertAtEnd(head,data);
+            return;
+        }
+      
+        newNode->next=temp->next;
+        temp->next->prev=newNode;
+        temp->next=newNode;
+        newNode->prev=temp;
+        // rough->prev=newNode;
         
-        if(temp==NULL)
-{
-            insertEnd(head,data);
-return;
-}
-        Node *nodeToInsert=new Node(data);
         
-        nodeToInsert->next=temp->next;
-        temp->next=nodeToInsert;
+        
     }
     
-     void deleteATFront(Node *&head)
-     {
-         head=head->next;
-         return;
-     }
-     
-     
-     void deleteATEnd(Node *&head)
-     {
-         Node *temp=head;
-
-         
-         while(temp->next!=NULL && temp->next->next!=NULL)
-         {
-            
-             temp=temp->next;
-             
-         }
-          temp->next=NULL;
-         return;
-     }
-     
-     
-     void deletATPos(Node *&head,int pos)
-     {
-         if(pos==1)
-         deleteATFront(head);
-         
-         Node *temp=head;
-         
-         int count=1;
-         while(count <pos-1)
-         {
-             count++;
-             temp=temp->next;
-         }
-         if(temp==NULL)
-         {
-             deleteATEnd(head);
-             return;
-         }
-         temp->next=temp->next->next;
-         
-     }
-    void printList(Node *head)
+    void deleteAtFront(Node *&head)
     {
-
-        Node *temp = head;
-        if (head == NULL)
-        {
-            "list is empty";
-        }
-
-        else
-        {
-            while (temp != NULL)
-            {
-                cout << temp->data << "  ";
-                temp = temp->next;
-            }
-        }
+        head->next->prev=NULL;
+        head=head->next;
     }
     
+    void deleteAtEnd(Node *&head)
+    {
+        Node *temp=head;
+        
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+            
+        }
+        temp->prev->next=NULL;
+        temp->prev=NULL;
+        
+    }
+    
+    void deletATPos(Node *&head,int pos)
+    {
+        if(pos==1)
+        {
+            deleteAtFront(head);
+            return;
+        }
+        int count=1;
+        Node *temp=head;
+        while(count <pos)
+        {
+            temp=temp->next;
+            count++;
+        }
+        if(temp==NULL || temp->next==NULL)
+        {
+            deleteAtEnd(head);
+            return;
+        }
+       temp->prev->next=temp->next;
+       temp->next->prev=temp->prev;
+        
+        
+        
+    }
+    
+    void print(Node *&head)
+    {
+        Node *temp=head;
+        if(head==NULL)
+        {
+            cout<<"empty";
+            return ;
+        }
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<" ";
+            
+            temp=temp->next;
+        }
+    }
 };
+int main() {
+Node *head=new Node(70);
+DoubllyLinkList a;
+a.insertATHead(head,10);
+a.insertAtEnd(head,90);
+a.insertATPos(head,60,2);
+a.deleteAtFront(head);
+a.deleteAtEnd(head);
+a.deletATPos(head,4);
+a.print(head);
 
-
-int main()
-{
-    LinkList a;
-   
-    Node *head = new Node(84);
-    a.insertAtHead(head, 83);
-    a.insertAtHead(head, 82);
-    a.insertAtHead(head, 81);
-    a.insertEnd(head,85);
-    a.insertAtPos(head,7,99);
-//     a.deleteATFront(head);
-//     a.deleteATEnd(head);
-    a.deletATPos(head,3);
-    a.printList(head);
-
-    return 0;
 }
